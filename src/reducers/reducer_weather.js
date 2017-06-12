abfde56a8f98f9bd9f6b5c0c9f52fb6e88e874a8
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-06-12 15:30:40 -0400

    Add new  component.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index'; 
+
+// params reference application state? 
+export default function(state = [], action) {
+	switch (action.type) {
+	case 'FETCH_WEATHER': 
+		// adds new city to existing application state?
+		return [action.payload.data, ...state];
+	}
 	return state; 
 }
