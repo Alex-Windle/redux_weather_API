eeaec63f010840d8494d85ecb54b6abe64014f0f
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-06-13 10:03:20 -0400

    Displays city name.

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,9 +1,43 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import { connect } from 'react-redux';  
 
-export default class WeatherList extends Component {
+// This component only displays data (from props)
+
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		return (
+			<tr key={cityData.city.id}>
+				<td>{cityData.city.name}</td>
+			</tr>
+		); 
+	}
+
 	render() {
 		return (
-			<div>Hello world!</div>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
 		); 
 	}
 }
+
+// Use CONNECT function to hook up component w/ state
+function mapStateToProps({ weather }) {
+	// const weather = state.weather
+
+	// ES6 --> shortcut to reference state
+	// return { weather: state.weather }
+	return { weather }; 
+}
+
+export default connect(mapStateToProps)(WeatherList);
