d22a38e1559a7d417a76ead12045046c51213b08
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-06-13 14:08:33 -0400

    Adds reference line for mean

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -2,13 +2,14 @@
 // --> FUNCTIONAL - because it takes props from its parent
 // and is not connected to state.
 import React from 'react'; 
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
 	return (
 		<td>
 			<Sparklines data={props.data}>
 				<SparklinesLine color={props.color} />
+				<SparklinesReferenceLine type="mean" />
 			</Sparklines>
 		</td>		
 	); 
